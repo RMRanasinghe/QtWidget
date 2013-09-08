9065a63843e6b01f7570c234188380b531cf1517
Rajitha <yrmranasinghe@gmail.com>
2013-09-08 22:32:11 +0530

    Initial layout design

--- Src/widget.cpp
+++ Src/widget.cpp
@@ -28,14 +28,4 @@ Widget::~Widget()
     delete ui;
 }
 
-void Widget::on_pushButton_clicked()
-{
-    communicator* cm =new communicator();
-    cm->processWrite("x=-10:0.01:10;");
-    cm->processWrite("y=exp(x);");
-    cm->processWrite("set(gcf,'Visible','off');");
-    cm->processWrite("plot(x,y);");
-    cm->processWrite("print -dpng /home/rama/s.png;");
-    cout << cm->processRead().toStdString();
-}
 
--- Src/widget.h
+++ Src/widget.h
@@ -15,9 +15,6 @@ public:
     explicit Widget(QWidget *parent = 0);
     ~Widget();
 
-private slots:
-    void on_pushButton_clicked();
-
 private:
     Ui::Widget *ui;
 };

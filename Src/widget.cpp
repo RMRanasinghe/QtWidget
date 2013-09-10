6bc0bd7890e29606706c26259c816447c405c694
Rajitha <yrmranasinghe@gmail.com>
2013-09-10 09:18:44 +0530

    added buttons

--- Src/widget.cpp
+++ Src/widget.cpp
@@ -14,6 +14,7 @@
 #include "communicator.h"
 #include "iostream"
 #include "graphic.h"
+#include <QComboBox>
 
 using namespace std;
 /*
@@ -59,6 +60,18 @@ void Widget::on_buttonBox_rejected()
 
 void Widget::on_pushButton_clicked()
 {
+    /*
+     *Y axis items. This should be changed and added. TODO: should change
+     *
+     */
+    ui->verticalLayout_2->addWidget(new QLabel("Y:"),0,Qt::AlignBottom);
+    ui->verticalLayout_2->addWidget(new QLineEdit(),0,Qt::AlignBottom);
+    ui->verticalLayout_2->addWidget(new QLabel("Label:"),0,Qt::AlignBottom);
+    ui->verticalLayout_2->addWidget(new QLineEdit(),0,Qt::AlignBottom);
+    ui->verticalLayout_2->addWidget(new QLabel("Color:"),0,Qt::AlignBottom);
+    ui->verticalLayout_2->addWidget(new QComboBox(),0,Qt::AlignBottom);
+    ui->verticalLayout_2->addWidget(new QLabel("Line Type:"),0,Qt::AlignBottom);
+    ui->verticalLayout_2->addWidget(new QComboBox(),0,Qt::AlignBottom);
     /*
      *Here verticalLayout_2 is the layout inside the scroll plane. TODO: all the widgets added to the
      *scorll plane should be added to this layout.

766b1d10f298d7e475a25ad04a5137477473336e
Rajitha <yrmranasinghe@gmail.com>
2013-09-21 13:27:30 +0530

    added line type and color options

--- Src/widgettreestruct.cpp
+++ Src/widgettreestruct.cpp
@@ -17,16 +17,59 @@
 
 widgetTreeStruct::widgetTreeStruct(QString variableName)
 {
-    tree = new QFormLayout;
+    tree = new QFormLayout; //tree structure for one graph
 
-    QLineEdit* equation =  new QLineEdit();
-    QLineEdit* label = new QLineEdit();
-    QComboBox* color = new QComboBox();
-    QComboBox* lineType = new QComboBox();
-    QString variable = variableName;
+    QLineEdit* equation =  new QLineEdit(); //test area for write equation
+    QLineEdit* label = new QLineEdit(); // label of the graph
+    QComboBox* color = new QComboBox(); //color selector
+    QComboBox* lineType = new QComboBox(); //line type selector
+    QString variable = variableName; //this variable name is the number of the graph
 
-    tree->addWidget(new QLabel(QString("Graph ").append(variableName).append(":")));
-    tree->addRow(variable,equation);
+    /*
+     *color selector menu.
+     */
+
+    //initialize pixmaps
+    QPixmap black(40,40);
+    QPixmap red(40,40);
+    QPixmap green(40,40);
+    QPixmap blue(40,40);
+    QPixmap magenta(40,40);
+    QPixmap cyan(40,40);
+
+    //fill colors
+    black.fill(Qt::black);
+    red.fill(Qt::red);
+    green.fill(Qt::green);
+    blue.fill(Qt::blue);
+    magenta.fill(Qt::magenta);
+    cyan.fill(Qt::cyan);
+
+    //add menu items
+    color->addItem(black,"        Black");
+    color->addItem(red,"        Red");
+    color->addItem(green,"        Green");
+    color->addItem(blue,"        Blue");
+    color->addItem(magenta,"        Magenta");
+    color->addItem(cyan,"        cyan");
+
+
+    /*
+     *Line type selector
+     */
+    lineType->addItem("_______________");
+    lineType->addItem("..............................");
+    lineType->addItem("*************");
+    lineType->addItem("ooooooooooooo");
+    lineType->addItem("xxxxxxxxxxxxxxx");
+    lineType->addItem("^^^^^^^^^^^");
+
+
+    /*
+     *add tree widgets to tree
+     */
+    tree->addWidget(new QLabel(QString("Graph ").append(variableName).append(":"))); //set the topic of tree
+    tree->addRow(new QLabel("y:"),equation);
     tree->addRow(new QLabel("Label:"),label);
     tree->addRow(new QLabel("Color:"),color);
     tree->addRow(new QLabel("Line Type:"),lineType);
